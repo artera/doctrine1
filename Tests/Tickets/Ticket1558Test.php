e9291d4789f5ccb602e012d8366de3528ecdf7f1
Massimiliano Torromeo <massimiliano.torromeo@gmail.com>
2021-04-30 16:04:14 +0200

    Fixed whereIn test

--- Tests/Tickets/Ticket1558Test.php
+++ Tests/Tickets/Ticket1558Test.php
@@ -15,7 +15,7 @@ class Ticket1558Test extends DoctrineUnitTestCase
             ->andWhereIn('u.id', [1])
             ->whereIn('u.id', [])
             ->orWhereIn('u.id', []);
-        $this->assertEquals($q->getSqlQuery(), 'SELECT e.id AS e__id, e.name AS e__name, e.loginname AS e__loginname, e.password AS e__password, e.type AS e__type, e.created AS e__created, e.updated AS e__updated, e.email_id AS e__email_id FROM entity e WHERE (e.id IN (?) OR e.id IN (?) OR e.id IN (?) AND e.id IN (?) AND (e.type = 0))');
+        $this->assertEquals($q->getSqlQuery(), 'SELECT e.id AS e__id, e.name AS e__name, e.loginname AS e__loginname, e.password AS e__password, e.type AS e__type, e.created AS e__created, e.updated AS e__updated, e.email_id AS e__email_id FROM entity e WHERE (e.id IN (?) OR e.id IN (?) OR e.id IN (?) AND e.id IN (?) AND e.id IN (?) AND (e.type = 0))');
         $q->execute();
     }
 }
